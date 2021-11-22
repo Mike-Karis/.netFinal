d4e3968d3a31ab38785872de0eb674e3d70af5f2
Michael karis <mkaris1@my.wctc.edu>
2021-11-22 08:37:34 -0600

    added nlog

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using NLog.Web;
+using System.IO;
 
 namespace _netFinal
 {
     class Program
     {
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
